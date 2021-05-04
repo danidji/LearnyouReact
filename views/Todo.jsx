4f43e969c8c478dc25428e4a3e3adf340d7ae492
dany gbelidji <dany.gbelidji@live.fr>
2021-05-04 09:28:29 +0200

    ✨ 05 - state

--- views/Todo.jsx
+++ views/Todo.jsx
@@ -3,19 +3,24 @@ import React from 'react';
 export class Todo extends React.Component {
     constructor(props) {
         super(props);
-        this.state = true;
+        this.state = {
+            checked: false
+        };
     }
 
+    handleChange(e) {
+        this.setState({ checked: e.target.checked })
+    }
 
     render() {
         return (
 
             <tr>
-                <td style={{ border: "1px solid black" }}>
+                <td style={{ border: "1px solid black;" }}>
                     <input type="checkbox" checked={this.state.checked} onChange={this.handleChange.bind(this)} />
                 </td>
-                <td style={{ border: "1px solid black" }}>{this.props.title}</td>
-                <td style={{ border: "1px solid black" }}>{this.props.children}</td>
+                <td style={{ border: "1px solid black;" }}>{this.props.title}</td>
+                <td style={{ border: "1px solid black;" }}>{this.props.children}</td>
             </tr>
         )
 
