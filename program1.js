3b8f7e14a9421af7aec60802121caf7516fcd675
dany gbelidji <dany.gbelidji@live.fr>
2021-05-05 23:14:31 +0200

    🚧 WIP - 08 - Isomorphic

--- program1.js
+++ program1.js
@@ -1,3 +1,6 @@
+var express = require('express');
+var app = express();
+
 var React = require('react');
 var ReactDOMServer = require('react-dom/server');
 var DOM = React.DOM;
@@ -8,10 +11,28 @@ var script = DOM.script;
 var browserify = require('browserify');
 var babelify = require("babelify");
 
-
-require('babel/register');
-
 var TodoBox = require('./views/index.jsx');
+
+app.set('port', (process.argv[2] || 4000));
+app.set('view engine', 'jsx');
+app.set('views', __dirname + '/views');
+app.engine('jsx', require('express-react-views').createEngine({ transformViews: false }));
+
+require('babel/register')({
+    ignore: false
+});
+
+// let data = [
+//     {
+//         title: "Shopping"
+//         , detail: process.argv[3]
+//     }
+//     , {
+//         title: "Hair cut"
+//         , detail: process.argv[4]
+//     }
+// ];
+
 
 app.use('/bundle.js', function (req, res) {
     res.setHeader('Content-Type', 'application/javascript');
@@ -39,4 +60,14 @@ app.use('/', function (req, res) {
     ));
 
     res.end(html);
+});
+
+
+// console.log(data)
+app.use('/', function (req, res) {
+    res.render('index', { data: data });
+});
+
+app.listen(app.get('port'), function () {
+    console.log('Express server is up on port 4000');
 });
